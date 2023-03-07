2b93c3c80e54e4f0a4228fe959b8fda994fe2c34
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-07 16:43:46 +0100

    import bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-export default{
+export default {
   name: 'App',
   components: {
-      AppHeader,
+    AppHeader,
   },
-  data(){
-    return{
+  data() {
+    return {
 
     }
   }
@@ -21,15 +21,20 @@ export default{
 </template>
 
 <style>
+@use './styles/general.scss' as *;
+@use 'bootstrap/scss/bootstrap' as *;
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
