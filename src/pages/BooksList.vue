b52e7d22673f8598fd1c8ce2b2221d9be576deef
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-07 16:19:34 +0100

    create 2 pages

---
+++ src/pages/BooksList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-center"> BookssList</h1>
+        <!-- <AppMain /> -->
+    </div>
+</template>
+<script>
+// import AppMain from '../components/AppMain.vue';
+export default {
+    name: 'BooksList',
+    components: {
+        // AppMain
+    }
+}
+</script>
+<style lang="scss" scoped></style>
